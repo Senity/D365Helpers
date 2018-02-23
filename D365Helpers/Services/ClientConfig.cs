1faf92acb0e92ad31da7aba3c6792f69ce5c5baa
Rajmund Zięciak <rajmund.zieciak@senity.pl>
2018-02-23 10:18:37 +0000

    Dodanie helperów dla simple custom service w D365

---
+++ D365Helpers/Services/ClientConfig.cs
@@ -0,0 +1,44 @@
+﻿
+namespace D365Helpers.Services
+{
+    class ClientConfig
+    {
+        public static ClientConfig Default { get { return ClientConfig.CloudAX; } }
+
+        public static ClientConfig CloudAX = new ClientConfig()
+        {
+
+            //UriString = "https://devaos.cloudax.dynamics.com/",
+            UriString = "",
+
+            // Insert the correct username here for the actual test (If needed).
+            UserName = "",
+            // Insert the correct password here for the actual test (If needed).
+            Password = "",
+
+            //ActiveDirectoryResource = "https://devaos.cloudax.dynamics.com",
+            ActiveDirectoryResource = "",
+            //ActiveDirectoryTenant = "https://login.windows.net/DOMAIN.com",
+            ActiveDirectoryTenant = "",
+            //ActiveDirectoryClientAppId = "0831b191-b463-41f6-a86c-041272bdb340",
+            ActiveDirectoryClientAppId = "",
+            // Insert here the application secret when authenticate with AAD by the application
+            //ActiveDirectoryClientAppSecret = "A/UcmIlmWWv7yhhV6/XokoQR582NQ/4/WUyuor0MbJQ=",
+            ActiveDirectoryClientAppSecret = "",
+
+            // Change TLS version of HTTP request from the client here
+            // Ex: TLSVersion = "1.2"
+            // Leave it empty if want to use the default version
+            TLSVersion = "",
+        };
+
+        public string TLSVersion { get; set; }
+        public string UriString { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ActiveDirectoryResource { get; set; }
+        public string ActiveDirectoryTenant { get; set; }
+        public string ActiveDirectoryClientAppId { get; set; }
+        public string ActiveDirectoryClientAppSecret { get; set; }
+    }
+}
---
+++ D365Helpers/Services/OAuthHelper.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+
+namespace D365Helpers.Services
+{
+    class OAuthHelper
+    {
+        /// <summary>
+        /// The header to use for OAuth authentication.
+        /// </summary>
+        public const string OAuthHeader = "Authorization";
+
+        /// <summary>
+        /// Retrieves an authentication header from the service.
+        /// </summary>
+        /// <returns>The authentication header for the Web API call.</returns>
+        public static string GetAuthenticationHeader(bool useWebAppAuthentication = false)
+        {
+            string aadTenant = ClientConfig.Default.ActiveDirectoryTenant;
+            string aadClientAppId = ClientConfig.Default.ActiveDirectoryClientAppId;
+            string aadClientAppSecret = ClientConfig.Default.ActiveDirectoryClientAppSecret;
+            string aadResource = ClientConfig.Default.ActiveDirectoryResource;
+
+            AuthenticationContext authenticationContext = new AuthenticationContext(aadTenant, false);
+            AuthenticationResult authenticationResult;
+
+            if (useWebAppAuthentication)
+            {
+                if (string.IsNullOrEmpty(aadClientAppSecret))
+                {
+                    throw new Exception("Failed OAuth by empty application secret. Fill CloudAX config first.");
+                }
+
+                try
+                {
+                    // OAuth through application by application id and application secret.
+                    var creadential = new ClientCredential(aadClientAppId, aadClientAppSecret);
+                    authenticationResult = authenticationContext.AcquireTokenAsync(aadResource, creadential).Result;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Failed to authenticate with Azure Active Directory by application.");
+                }
+            }
+            else
+            {
+                // OAuth through username and password.
+                string username = ClientConfig.Default.UserName;
+                string password = ClientConfig.Default.Password;
+
+                if (string.IsNullOrEmpty(password))
+                {
+                    throw new Exception("Failed OAuth by empty password. Fill CloudAX config first.");
+                }
+
+                try
+                {
+                    // Get token object
+                    var userCredential = new UserPasswordCredential(username, password); ;
+                    authenticationResult = authenticationContext.AcquireTokenAsync(aadResource, aadClientAppId, userCredential).Result;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Failed to authenticate with Azure Active Directory by the credential.");
+                }
+            }
+
+            // Create and get JWT token
+            return authenticationResult.CreateAuthorizationHeader();
+        }
+    }
+}
